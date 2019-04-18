0d81c9f9dc26decb0ea725d10461f145edafff87
Michel <github@chromadigital.group>
2019-04-18 06:17:36 +0200

    added CreateRemoteThreadEx

--- source/ProcessMemoryUtilities/Memory/ProcessMemory.cs
+++ source/ProcessMemoryUtilities/Memory/ProcessMemory.cs
@@ -1,8 +1,8 @@
 ﻿#pragma warning disable IDE0060 // Remove unused parameter
 
 using System;
-using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 using ProcessMemoryUtilities.PInvoke;
 
@@ -15,6 +15,9 @@ namespace ProcessMemoryUtilities.Memory
     {
         // https://gist.github.com/michel-pi/361f1f8bdca51235cb97aba0256d47e9
         private const int NT_STATUS_INFORMATION_MAX = 2147483647;
+
+        private static readonly IntPtr _kernelCreateRemoteThreadEx;
+        private static readonly IntPtr _kernelOpenProcess;
 
         private static readonly IntPtr _ntAllocateVirtualMemory;
         private static readonly IntPtr _ntClose;
@@ -57,6 +60,11 @@ namespace ProcessMemoryUtilities.Memory
 
         static ProcessMemory()
         {
+            var kernel = DynamicImport.ImportLibrary("kernel32.dll");
+
+            _kernelCreateRemoteThreadEx = DynamicImport.ImportMethod(kernel, "CreateRemoteThreadEx");
+            _kernelOpenProcess = DynamicImport.ImportMethod(kernel, "OpenProcess");
+
             var ntdll = DynamicImport.ImportLibrary("ntdll.dll");
 
             _ntAllocateVirtualMemory = DynamicImport.ImportMethod(ntdll, "NtAllocateVirtualMemory");
@@ -125,6 +133,107 @@ namespace ProcessMemoryUtilities.Memory
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThreadEx(IntPtr handle, IntPtr startAddress)
+        {
+            Ldarg(nameof(handle));
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldarg(nameof(startAddress));
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldc_I4((int)ThreadCreationFlags.Immediately);
+            Conv_U4();
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldsfld(new FieldRef(typeof(ProcessMemory), nameof(_kernelCreateRemoteThreadEx)));
+            Calli(new StandAloneMethodSig(CallingConvention.StdCall, typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(uint), typeof(IntPtr), typeof(IntPtr)));
+
+            return IL.Return<IntPtr>();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThreadEx(IntPtr handle, IntPtr startAddress, IntPtr parameter)
+        {
+            Ldarg(nameof(handle));
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldarg(nameof(startAddress));
+
+            Ldarg(nameof(parameter));
+
+            Ldc_I4((int)ThreadCreationFlags.Immediately);
+            Conv_U4();
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldsfld(new FieldRef(typeof(ProcessMemory), nameof(_kernelCreateRemoteThreadEx)));
+            Calli(new StandAloneMethodSig(CallingConvention.StdCall, typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(uint), typeof(IntPtr), typeof(IntPtr)));
+
+            return IL.Return<IntPtr>();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThreadEx(IntPtr handle, IntPtr threadAttributes, IntPtr stackSize, IntPtr startAddress, IntPtr parameter, ThreadCreationFlags creationFlags, IntPtr attributeList)
+        {
+            Ldarg(nameof(handle));
+            Ldarg(nameof(threadAttributes));
+            Ldarg(nameof(stackSize));
+            Ldarg(nameof(startAddress));
+            Ldarg(nameof(parameter));
+            Ldarg(nameof(creationFlags));
+            Conv_U4();
+            Ldarg(nameof(attributeList));
+
+            Ldc_I4_0();
+            Conv_I();
+
+            Ldsfld(new FieldRef(typeof(ProcessMemory), nameof(_kernelCreateRemoteThreadEx)));
+            Calli(new StandAloneMethodSig(CallingConvention.StdCall, typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(uint), typeof(IntPtr), typeof(IntPtr)));
+
+            return IL.Return<IntPtr>();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThreadEx(IntPtr handle, IntPtr threadAttributes, IntPtr stackSize, IntPtr startAddress, IntPtr parameter, ThreadCreationFlags creationFlags, IntPtr attributeList, ref uint threadId)
+        {
+            Ldarg(nameof(handle));
+            Ldarg(nameof(threadAttributes));
+            Ldarg(nameof(stackSize));
+            Ldarg(nameof(startAddress));
+            Ldarg(nameof(parameter));
+            Ldarg(nameof(creationFlags));
+            Conv_U4();
+            Ldarg(nameof(attributeList));
+            Ldarg(nameof(threadId));
+
+            Ldsfld(new FieldRef(typeof(ProcessMemory), nameof(_kernelCreateRemoteThreadEx)));
+            Calli(new StandAloneMethodSig(CallingConvention.StdCall, typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(IntPtr), typeof(uint), typeof(IntPtr), typeof(IntPtr)));
+
+            return IL.Return<IntPtr>();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IntPtr OpenProcess(ProcessAccessFlags desiredAccess, int processId)
         {
             IL.DeclareLocals(
@@ -170,6 +279,21 @@ namespace ProcessMemoryUtilities.Memory
             IL.MarkLabel("success");
 
             Ldloc("handle");
+            return IL.Return<IntPtr>();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr OpenProcess(ProcessAccessFlags desiredAccess, bool inheritHandle, int processId)
+        {
+            Ldarg(nameof(desiredAccess));
+            Ldarg(nameof(inheritHandle));
+            Conv_I4();
+            Ldarg(nameof(processId));
+            Conv_U4();
+
+            Ldsfld(new FieldRef(typeof(ProcessMemory), nameof(_kernelOpenProcess)));
+            Calli(new StandAloneMethodSig(CallingConvention.StdCall, typeof(IntPtr), typeof(uint), typeof(int), typeof(uint)));
+
             return IL.Return<IntPtr>();
         }
 
@@ -390,18 +514,9 @@ namespace ProcessMemoryUtilities.Memory
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool VirtualProtectEx(IntPtr handle, IntPtr address, IntPtr size, MemoryProtectionFlags newProtect, out MemoryProtectionFlags oldProtect)
+        public static bool VirtualProtectEx(IntPtr handle, IntPtr address, IntPtr size, MemoryProtectionFlags newProtect, ref MemoryProtectionFlags oldProtect)
         {
             IL.DeclareLocals(new LocalVar("regionSize", typeof(uint)));
-
-            Ldc_I4_0();
-            Ldc_I4_0();
-            Ceq();
-            Brtrue("jump");
-
-            oldProtect = 0;
-
-            IL.MarkLabel("jump");
 
             Ldarg(nameof(size));
             Conv_U4();
