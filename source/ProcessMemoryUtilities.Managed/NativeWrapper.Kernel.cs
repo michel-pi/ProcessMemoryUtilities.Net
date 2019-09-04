b76c2bc486f98d21a02bc198c48f4402a8827cf9
michel-pi <github@michel-pi.de>
2019-09-05 00:00:23 +0200

    added Kernel32 wrapper

---
+++ source/ProcessMemoryUtilities.Managed/NativeWrapper.Kernel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using ProcessMemoryUtilities.Native;
+
+namespace ProcessMemoryUtilities.Managed
+{
+    public static partial class NativeWrapper
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThread(
+            IntPtr handle,
+            IntPtr threadAttributes,
+            IntPtr stackSize,
+            IntPtr startAddress,
+            IntPtr parameter,
+            ThreadCreationFlags creationFlags,
+            IntPtr threadId)
+            => Kernel32.CreateRemoteThreadEx(handle, threadAttributes, stackSize, startAddress, parameter, creationFlags, IntPtr.Zero, threadId);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThread(
+            IntPtr handle,
+            IntPtr threadAttributes,
+            IntPtr stackSize,
+            IntPtr startAddress,
+            IntPtr parameter,
+            ThreadCreationFlags creationFlags,
+            out uint threadId)
+            => Kernel32.CreateRemoteThreadEx(handle, threadAttributes, stackSize, startAddress, parameter, creationFlags, IntPtr.Zero, out threadId);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThread(
+            IntPtr handle,
+            IntPtr startAddress,
+            IntPtr parameter,
+            ThreadCreationFlags creationFlags)
+            => Kernel32.CreateRemoteThreadEx(handle, startAddress, parameter, creationFlags);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThread(
+            IntPtr handle,
+            IntPtr startAddress,
+            IntPtr parameter,
+            ThreadCreationFlags creationFlags,
+            out uint threadId)
+            => Kernel32.CreateRemoteThreadEx(handle, startAddress, parameter, creationFlags, out threadId);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr CreateRemoteThread(
+            IntPtr handle,
+            IntPtr startAddress,
+            IntPtr parameter)
+            => Kernel32.CreateRemoteThreadEx(handle, startAddress, parameter);
+    }
+}
