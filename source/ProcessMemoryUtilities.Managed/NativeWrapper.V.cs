38c928970a7c153ffd5792be4ea6a524c5dd464a
michel-pi <github@michel-pi.de>
2019-09-05 00:00:08 +0200

    added virtual memory operations

---
+++ source/ProcessMemoryUtilities.Managed/NativeWrapper.V.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using ProcessMemoryUtilities.Native;
+
+using static ProcessMemoryUtilities.Native.NtDll;
+
+namespace ProcessMemoryUtilities.Managed
+{
+    public static partial class NativeWrapper
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr VirtualAllocEx(IntPtr handle, IntPtr address, IntPtr size, AllocationType allocationType, MemoryProtectionFlags memoryProtection)
+        {
+            uint status = NtAllocateVirtualMemory(handle, size, allocationType, memoryProtection, out address);
+
+            if (CaptureErrors) LastError = (int)status;
+
+            return NtSuccess(status) ? address : IntPtr.Zero;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool VirtualFreeEx(IntPtr handle, IntPtr address, IntPtr size, FreeType freeType)
+        {
+            uint status = NtFreeVirtualMemory(handle, address, size, freeType);
+
+            if (CaptureErrors) LastError = (int)status;
+
+            return NtSuccess(status);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool VirtualProtectEx(IntPtr handle, IntPtr address, IntPtr size, MemoryProtectionFlags newProtect, out MemoryProtectionFlags oldProtect)
+        {
+            uint status = NtProtectVirtualMemory(handle, address, size, newProtect, out oldProtect);
+
+            if (CaptureErrors) LastError = (int)status;
+
+            return NtSuccess(status);
+        }
+    }
+}
