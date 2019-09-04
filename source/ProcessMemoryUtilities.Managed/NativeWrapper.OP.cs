99425d8603edbc76c70f8dd7b85417a3f9395514
michel-pi <github@michel-pi.de>
2019-09-05 00:06:40 +0200

    added documentation

--- source/ProcessMemoryUtilities.Managed/NativeWrapper.OP.cs
+++ source/ProcessMemoryUtilities.Managed/NativeWrapper.OP.cs
@@ -9,6 +9,11 @@ namespace ProcessMemoryUtilities.Managed
 {
     public static partial class NativeWrapper
     {
+        /// <summary>
+        /// Closes an open object handle.
+        /// </summary>
+        /// <param name="handle">A valid handle to an open object.</param>
+        /// <returns>If the function succeeds, the return value is nonzero. If the function fails, the return value is zero.To get extended error information, call GetLastError.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool CloseHandle(IntPtr handle)
         {
@@ -19,6 +24,13 @@ namespace ProcessMemoryUtilities.Managed
             return NtSuccess(status);
         }
 
+        /// <summary>
+        /// Opens an existing local process object.
+        /// </summary>
+        /// <param name="desiredAccess">The access to the process object. This access right is checked against the security descriptor for the process.</param>
+        /// <param name="inheritHandle">If this value is TRUE, processes created by this process will inherit the handle. Otherwise, the processes do not inherit this handle.</param>
+        /// <param name="processId">The identifier of the local process to be opened.</param>
+        /// <returns>If the function succeeds, the return value is an open handle to the specified process. If the function fails, the return value is IntPtr.Zero. To get extended error information, call GetLastError.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IntPtr OpenProcess(ProcessAccessFlags desiredAccess, bool inheritHandle, int processId)
         {
@@ -29,6 +41,12 @@ namespace ProcessMemoryUtilities.Managed
             return handle;
         }
 
+        /// <summary>
+        /// Opens an existing local process object.
+        /// </summary>
+        /// <param name="desiredAccess">The access to the process object. This access right is checked against the security descriptor for the process.</param>
+        /// <param name="processId">The identifier of the local process to be opened.</param>
+        /// <returns>If the function succeeds, the return value is an open handle to the specified process. If the function fails, the return value is IntPtr.Zero. To get extended error information, call GetLastError.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IntPtr OpenProcess(ProcessAccessFlags desiredAccess, int processId)
         {
