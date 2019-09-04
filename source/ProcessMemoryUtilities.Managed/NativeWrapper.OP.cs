7642fa7be36905cee40b11fcc0a85c0c668adf52
michel-pi <github@michel-pi.de>
2019-09-05 00:00:15 +0200

    added OpenProcess and CloseHandle

---
+++ source/ProcessMemoryUtilities.Managed/NativeWrapper.OP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using ProcessMemoryUtilities.Native;
+
+using static ProcessMemoryUtilities.Native.NtDll;
+
+namespace ProcessMemoryUtilities.Managed
+{
+    public static partial class NativeWrapper
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool CloseHandle(IntPtr handle)
+        {
+            uint status = NtClose(handle);
+
+            if (CaptureErrors) LastError = (int)status;
+
+            return NtSuccess(status);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr OpenProcess(ProcessAccessFlags desiredAccess, bool inheritHandle, int processId)
+        {
+            uint status = NtOpenProcess(desiredAccess, inheritHandle, processId, out var handle);
+
+            if (CaptureErrors) LastError = (int)status;
+
+            return handle;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr OpenProcess(ProcessAccessFlags desiredAccess, int processId)
+        {
+            uint status = NtOpenProcess(desiredAccess, processId, out var handle);
+
+            if (CaptureErrors) LastError = (int)status;
+
+            return handle;
+        }
+    }
+}
